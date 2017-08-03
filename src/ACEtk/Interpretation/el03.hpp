58ac91e47573d55cab31c89c802deab4a4ff6575
David Dixon <ddixon@chili.lanl.gov>
2017-08-03 11:07:56 -0600

    refactoring Interpretation in progress

--- src/ACEtk/Interpretation/el03.hpp
+++ src/ACEtk/Interpretation/el03.hpp
@@ -3,56 +3,17 @@ class Interpretation< El03 >{
 
   const Table& table;
 
+  static auto Z( const Table& table ){ return table.data.NXS( 2 ); }
+
+#include "ACEtk/Interpretation/el03/RadiativeStoppingPower.hpp"
+
 public:
   Interpretation( const Table& table ) : table( table ) {}
 
+  auto atomicNumber() const { return Z( this->table ); }
 
-  
-  template< typename Tag >
-  auto energyGrid( const Tag ) const {
-    return table.data.XSS( table.data.JXS( Tag::energyGridBegin ),
-			   table.data.NXS( Tag::energyGridLength ) )
-      | ranges::view::transform( []( const auto& entry ){ return entry * mega( electronVolt ); } );
+  auto radiativeStoppingPower() const {
+    return RadiativeStoppingPower( this->table );
   }
-
-  auto bremsstrahlungCorrection() const {  
-    auto length = table.data.NXS( 3 );
-    auto start  = table.data.JXS( 2 ) + 2 * length;
-    return table.data.XSS( start, length );
-  }
-  
-  auto atomicNumber() const {
-    return table.data.NXS( 2 );
-  }
-
-  auto radiativeStoppingPower( ) const {
-
-    const auto atomicNumber = this->atomicNumber();
-
-    constexpr Quantity< Mega< ElectronVolt > > massEquivalent =
-      constant::electronMass * constant::lightSpeed * constant::lightSpeed;
-    
-    auto totalEnergy = this->energyGrid( El03::RadiativeStopping{} )
-      | ranges::view::transform( [massEquivalent]( const auto& entry )
-				 { return entry + massEquivalent; } );
-
-    constexpr auto multiplier = constant::classicalElectronRadius
-                              * constant::classicalElectronRadius
-                              * constant::fineStructure ;
-
-    auto predecate = [m=multiplier, z=atomicNumber]( const auto& tup )
-      { return m * z * ( z + std::get<0>(tup) ) * std::get<1>(tup) * std::get<2>(tup); };      
-
-    auto length = table.data.NXS( 3 );
-    auto start  = table.data.JXS( 2 ) + length;
-    auto radiativeStopping = table.data.XSS( start, length );
-    
-    return ranges::view::zip( this->bremsstrahlungCorrection(),
-			      radiativeStopping,
-			      totalEnergy ) 
-      | ranges::view::transform( predecate );
-
-  }
-
 };
   
